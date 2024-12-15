c5b7db3ff205181b511c1cedc6c9a8de5252ac94
AppVeyor <yask.alexandr@gmail.com>
2024-12-15 21:42:38 +0300

    2

--- src/components/mainMenu/bookingmenu/searhtrain.jsx
+++ src/components/mainMenu/bookingmenu/searhtrain.jsx
@@ -2,9 +2,39 @@ import "./searchtrain.css"
 
 export default function SearchTrain (){
     return (
-    <div className="searchTrain">
+    <form className="formSearchTrain">
+        <div className="searchTrain">
 
-    </div>
+            <div className="headword">
+                 <span>Направление</span>
+            </div>
+            <div className="inputsway">
+                <div className="inputfromway">
+                    <input         
+                    type="text"/>
+                </div>
+                <div className="inputwhereway">
+                    <input 
+                    type="text"/>
+                </div>
+            </div>
+
+            <div className="headword">
+                 <span>Дата</span>
+            </div>
+            <div className="inputsway">
+                <div className="inputfromdata">
+                    <input         
+                    type="text"/>
+                </div>
+                <div className="inputwheredata">
+                    <input 
+                    type="text"/>
+                </div>
+            </div>
+
+        </div>
+    </form>
     )
 
 }
