31534eae947c1c9e605af22d7a17474c23deba8a
AppVeyor <yask.alexandr@gmail.com>
2025-02-23 21:10:49 +0300

    add linebar

--- src/components/searchresults/searchresults.jsx
+++ src/components/searchresults/searchresults.jsx
@@ -3,10 +3,28 @@ import React from 'react';
 import { ProgressLine } from "./lineprogress/lineprogres";
 import DataFrom from "../calendar/dataFrom";
 import DataTo from "../calendar/dataTo";
+import { useState } from "react";
 
 
 function SearchResults() {
+  const [sliderValueMin, setSliderValueMin] = useState(1920); 
+  const [sliderValueMax, setSliderValueMax] = useState(7000); 
 
+  const handleSliderChangeMin = (event) => {
+    const value = parseInt(event.target.value, 10);
+    if (value <= sliderValueMax) {
+        setSliderValueMin(value);
+    }
+};
+
+const handleSliderChangeMax = (event) => {
+    const value = parseInt(event.target.value, 10);
+    if (value >= sliderValueMin) {
+        setSliderValueMax(value);
+    }
+};
+const minPercentage = ((sliderValueMin - 1920) / (7000 - 1920)) * 100;
+const widthPercentage = ((sliderValueMax - sliderValueMin) / (7000 - 1920)) * 100;
 
   return (
     <div className='searchresult'>
@@ -81,7 +99,48 @@ function SearchResults() {
                   </div>
 
               </div>
-              
+
+              <div className="line"></div>
+
+              <div className="cost">
+                  <div className="textcost">Стоимость</div>
+                  <div className="texts">
+                    <span className="textstart">от</span>
+                    <span className="textend">до</span>
+                  </div>
+                  <div className="inputs">
+                    
+                      <input
+                          className="rangemin"
+                          type="range"
+                          min="1920"
+                          max="7000"
+                          value={sliderValueMin}
+                          onChange={handleSliderChangeMin}
+                            />
+
+                      <input
+                          className="rangemax"
+                          type="range"
+                          min="1920"
+                          max="7000"
+                          value={sliderValueMax}
+                          onChange={handleSliderChangeMax}
+                           />
+                                   <div
+            className="ant-slider-track ant-slider-track-1"
+            style={{
+              left: `${minPercentage}%`,
+              width: `${widthPercentage}%`,
+            }}
+          />
+                        </div>
+                        <div className="spans">
+                          <span className="slider-valuemin">{sliderValueMin}</span>
+                          <span className="slider-valuemax">{sliderValueMax}</span>
+                        </div>
+                     
+              </div>
       </div>
     </div>
   );
