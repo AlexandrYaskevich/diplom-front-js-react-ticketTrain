99fb1454c9009be2dc79f9f2ff6cb6d51dc576f2
AppVeyor <yask.alexandr@gmail.com>
2025-04-06 22:59:50 +0300

    get app tickekts2

--- src/components/searchresults/resulttickets.jsx
+++ src/components/searchresults/resulttickets.jsx
@@ -38,73 +38,55 @@ useEffect(() => {
         const isExpressAvailable = !searchParamsForTickets.have_express || ticket.is_express; // Используем is_express
         const isDepartureTimeValid = ticketDepartureTime >= searchParamsForTickets.start_departure_hour_from && ticketDepartureTime <= searchParamsForTickets.start_departure_hour_to;
 
-        return isWithinDateRange && isPriceValid && isClassValid && isWiFiAvailable && isExpressAvailable && isDepartureTimeValid;
+        return isWithinDateRange || isPriceValid || isClassValid || isWiFiAvailable || isExpressAvailable || isDepartureTimeValid;
     }));
 
     console.log(filteredTickets);
 }, [tickets, searchParamsForTickets]);
     return(
         <div>
-            <div>
-                <div>From City ID: {searchParamsForTickets.from_city_id}</div>
-                <div>To City ID: {searchParamsForTickets.to_city_id}</div>
-                <div>From City NAME: {searchParamsForTickets.from_city_name}</div>
-                <div>To City NAME: {searchParamsForTickets.to_city_name}</div>
-                <div>Date Start: {searchParamsForTickets.date_start}</div>
-                <div>Date End: {searchParamsForTickets.date_end}</div>
-                <div>Class2: {typeof searchParamsForTickets.have_second_class === 'boolean' ? String(searchParamsForTickets.have_second_class) : ''}</div>
-                <div>Class1: {typeof searchParamsForTickets.have_first_class === 'boolean' ? String(searchParamsForTickets.have_first_class) : ''}</div>
-                <div>Class3: {typeof searchParamsForTickets.have_third_class === 'boolean' ? String(searchParamsForTickets.have_third_class) : ''}</div>
-                <div>Class4: {typeof searchParamsForTickets.have_fourth_class === 'boolean' ? String(searchParamsForTickets.have_fourth_class) : ''}</div>
-                <div>Wifi: {typeof searchParamsForTickets.have_wifi === 'boolean' ? String(searchParamsForTickets.have_wifi) : ''}</div>
-                <div>Express: {typeof searchParamsForTickets.have_express === 'boolean' ? String(searchParamsForTickets.have_express) : ''}</div>
-                <div>PriceFrom: {searchParamsForTickets.price_from}</div>
-                <div>PriceTo:{searchParamsForTickets.price_to}</div>
-                <div>TimeFrom: {searchParamsForTickets.start_departure_hour_from}</div>
-                <div>TimeTo: {searchParamsForTickets.start_departure_hour_to}</div>
-            </div>
-
             <div className="ticketslist">
-            {tickets.map((ticket) => (   
+            {filteredTickets.map((ticket) => (   
                 <div className="ticket" key={uuidv4()}>
                     <div className="nameTrainPart">
                         <div className="circle">
                             <div className="trainicon"></div>
                         </div>
                         <div className="wayName">
-                            <div className="tarinName">Название поезда</div>
+                            <div className="tarinName">{ticket.departure.train.name}</div>
                             <div className="citylist">
                                 <div className="fistCity">
-                                    <div className="citys">города</div>
+                                    <div className="citys">{ticket.departure.from.city.name}</div>
                                     <p>&#8594;</p>
                                 </div>
-                                <div className="citys">города</div>
+                                <div className="citys">{ticket.departure.to.city.name}</div>
                             </div>
                         </div>
                     </div>
                     <div className="timeTrainPart">
                         <div className="midlepart">
                             <div className="fromWay">
-                                <div className="timefrom">0000</div>
-                                <div className="cityfrom">город</div>
-                                <div className="namestationfrom">станция</div>
+                                <div className="timefrom">{new Date(ticket.departure.from.datetime * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</div>
+                                <div className="cityfrom">{ticket.departure.from.city.name}</div>
+                                <div className="namestationfrom">{ticket.departure.from.railway_station_name}</div>
                             </div>
                             <div className="alltimeway">
-                                <div className="alltime">0000</div>
+                                <div className="alltime">{Math.floor((ticket.departure.to.datetime - ticket.departure.from.datetime) / 3600)} : {Math.floor(((ticket.departure.to.datetime - ticket.departure.from.datetime) % 3600) / 60)}</div>
                                 <div className="iconarrow"></div>
                             </div>
                             <div className="toWay">
-                                <div className="timeto">0000</div>
-                                <div className="cityto">город</div>
-                                <div className="namestationto">станция</div>
+                                <div className="timeto">{new Date(ticket.departure.to.datetime * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</div>
+                                <div className="cityto">{ticket.departure.to.city.name}</div>
+                                <div className="namestationto">{ticket.departure.to.railway_station_name}м</div>
                             </div>
                         </div>    
                     </div>
                     <div className="seatsTrainPart">
                         <div className="listclassticket">
                             <div className="classseat">
-                                <div className="nameclass">Примместа</div>
-                                <div className="amountseats">00</div>
+
+                                <div className="nameclass">класс</div>
+                                <div className="amountseats">{ticket.available_seats}</div>
                                 <div className="costseats">
                                     <div className="fromcost">от</div>
                                     <div className="amountcost">{ticket.min_price}</div>
